0ba5ca1c4c6ab2ab4d3d243b98ba527f702b0196
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-05-18 04:34:37 +0530

    inverted-sort-fn-qtlist.js

--- section-20-project/quote-forum/src/components/quotes/QuoteList.js
+++ section-20-project/quote-forum/src/components/quotes/QuoteList.js
@@ -7,9 +7,9 @@ import classes from './QuoteList.module.css';
 const sortQuotes = (quotes, ascending) => {
   return quotes.sort((quoteA, quoteB) => {
     if (ascending) {
-      return quoteA.id > quoteB.id ? 1 : -1;
-    } else {
       return quoteA.id < quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id > quoteB.id ? 1 : -1;
     }
   });
 };

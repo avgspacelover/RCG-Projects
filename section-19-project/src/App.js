ae7288c8a2be3053165feac775d289db779c1d4f
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2022-08-17 13:36:25 +0530

    edits

---
+++ section-19-project/src/App.js
@@ -0,0 +1,66 @@
+import { Fragment, useEffect } from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+// import { uiActions } from './store/ui-slice';
+import {sendCartData, fetchCartData} from './store/cart-actions'
+
+let isInitial= true;
+
+
+function App() {
+
+  const showCart = useSelector(state => state.ui.cartIsVisible);
+
+  const cart = useSelector(state=> state.cart);
+
+  const notification = useSelector(state=> state.ui.notification);
+
+
+  const dispatch = useDispatch();
+  
+  useEffect( () => {
+    dispatch(fetchCartData())
+
+  }, [dispatch]);
+  
+
+  useEffect( () => {
+  
+
+    if(isInitial){
+      isInitial=false;
+      return;
+    }
+    
+    if(cart.changed){
+      
+      dispatch(sendCartData(cart))
+
+    }
+
+
+  },[cart, dispatch]);
+
+  return (
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+    </Layout>
+
+    </Fragment>
+    
+  );
+}
+
+export default App;
